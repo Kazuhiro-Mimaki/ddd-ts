9487e98b8de0560846736a76405c66ff1107ee92
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-11-08 23:11:47 +0900

    feat: chapter 5, in memory repository

--- src/repository/user.ts
+++ src/repository/user.ts
@@ -2,5 +2,27 @@ import { User } from "../domain";
 
 export interface IUserRepository {
   save(user: User): void;
-  find(userName: string): User;
+  findByName(userName: string): User | undefined;
+}
+
+export class InMemoryUserRepository implements IUserRepository {
+  public store: User[];
+
+  constructor() {
+    this.store = this.init();
+  }
+
+  private init(): User[] {
+    return Array.from(Array(10), (v) => {
+      return new User({ id: v, name: `User${v}` });
+    });
+  }
+
+  public findByName(userName: string): User | undefined {
+    return this.store.find((v) => v.name === userName);
+  }
+
+  public save(user: User) {
+    this.store.push(user);
+  }
 }
